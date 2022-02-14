0a27837f5a6974998e57381ff3968fd08111d6ff
Seminar Bioinformatik <neugchr>
2022-02-14 10:15:58 +0100

    made some changes to lists.py

---
+++ lists.py
@@ -0,0 +1,38 @@
+from math import log
+
+weekdays = ['monday',
+            'tuesday',
+            'wednesday',
+            'thursday',
+            'friday',
+            'saturday'
+            'sunday']
+
+
+a_frag = ['1','2','3']
+b_frag = ['a','b','c']
+c_frag = ['α','β','γ']
+
+squared = []
+loged = []
+for i in a_frag:
+    squared.append(int(i)*int(i))
+    loged.append(log(int(i)))
+
+print('i\t1\t2\t3')
+print('i^2\t'+str(squared[0])+'\t'+str(squared[1])+'\t'+str(squared[2]))
+print('log(i)\t'+str(loged[0])+'\t'+str(loged[1])+'\t'+str(loged[2]))
+
+for a in a_frag:
+    for b in b_frag:
+        for c in c_frag:
+            print(a+b+c)
+
+masterlist = []
+for i in squared:
+    masterlist.append(i)
+for b in b_frag:
+    masterlist.append(b)
+
+print("the master list is\n")
+print(masterlist)
