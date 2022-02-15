3363ac02283b3089ea57ad645effac11290e34c5
Seminar Bioinformatik <neugchr>
2022-02-15 13:27:34 +0100

    renamed exercise15 to matrix

--- matrix.py
+++ matrix.py
@@ -46,14 +46,33 @@ class Matrix:
     def print(self):
         print(self.__str__())
 
-inp = input("create matrix with dimensions x,y:\n")
-inp = inp.split(",")
-inp = [int(item) for item in inp]
+    def del_col(self, x):
+        self.data.remove(self.data[x])
+        self.x_dim -=1
 
-m = Matrix(inp[0], inp[1])
+    def del_row(self, y):
+        for sublist in self.data:
+            sublist.remove(sublist[y])
+        self.y_dim -=1
 
-for x in range(m.x_dim):
-    for y in range(m.y_dim):
-        m.set(x,y, randint(1,100))
+    def populate(self):
+        for x in range(self.x_dim):
+            for y in range(self.y_dim):
+                #self.set(x,y, chr(randint(192,600)))
+                self.set(x,y, randint(192, 600))
 
-m.print()
+##################################
+# SCRIPT
+##################################
+
+# inp = input("create matrix with dimensions x,y:\n")
+# inp = inp.split(",")
+# inp = [int(item) for item in inp]
+#
+# m = Matrix(inp[0], inp[1])
+#
+# for x in range(m.x_dim):
+#     for y in range(m.y_dim):
+#         m.set(x,y, chr(randint(80,500)))
+#
+# m.print()
